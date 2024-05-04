30e6005fded944e0abb282140ae370405759ae46
vararais <varadigmaisnann@gmail.com>
2024-05-04 12:00:03 +0700

    Menambah Output dan If else

--- Struct/Struct.cpp
+++ Struct/Struct.cpp
@@ -178,7 +178,14 @@ void searchData() {
 	cout << "\nEnter the roll number of the student whose record you want to search:";
 	int num;
 	cin >> num;
-	
+	if (search(num, &prev, &curr) == false)
+		cout << "\nrecord not Found" << endl;
+	else
+	{
+		cout << "\nRecord found" << endl;
+		cout << "\nRoll number : " << curr->noMhs << endl;
+		cout << "\nName : " << curr->name << endl;
+	}
 }
 int main()
 {
