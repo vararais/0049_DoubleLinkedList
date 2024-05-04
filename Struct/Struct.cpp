901e2f4d9c55e87c859b143cbc22bc3a9d480842
vararais <varadigmaisnann@gmail.com>
2024-05-04 11:57:34 +0700

    Menambahkan Elemen dalam Search Data

--- Struct/Struct.cpp
+++ Struct/Struct.cpp
@@ -169,7 +169,16 @@ void revtraverse() {
 }
 
 void searchData() {
-
+	if (listEmpty() == true)
+	{
+		cout << "\nList is empty" << endl;
+	}
+	Node* prev, * curr;
+	prev = curr = NULL;
+	cout << "\nEnter the roll number of the student whose record you want to search:";
+	int num;
+	cin >> num;
+	
 }
 int main()
 {
