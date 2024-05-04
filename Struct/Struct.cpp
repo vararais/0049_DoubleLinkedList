8704dafdb0274b4ba9b5a7b78e54423b9d086224
vararais <varadigmaisnann@gmail.com>
2024-05-04 11:19:08 +0700

    Menambah If

--- Struct/Struct.cpp
+++ Struct/Struct.cpp
@@ -87,6 +87,12 @@ void deleteNode() {
 
 	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
 	cin >> rollNo;
+	
+	if (START == NULL)
+	{
+		cout << "List is empty" << endl;
+		return;
+	}
 }
 
 int main()
