670d7bfb5ca2bd8ced2c43d37717b4dba6923e25
vararais <varadigmaisnann@gmail.com>
2024-05-04 11:31:33 +0700

    Menmabah Delete Current

--- Struct/Struct.cpp
+++ Struct/Struct.cpp
@@ -126,6 +126,9 @@ void deleteNode() {
 			current->next->prev = previous;
 		}
 	}
+
+	delete current;
+	cout << "\x1b[32mRecord with roll number " << rollNo << "deleted\x1b[0m" << endl;
 }
 
 int main()
