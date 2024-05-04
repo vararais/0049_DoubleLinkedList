6f007e7fe7cf5d323477f0fd2b26ccd93c5af181
vararais <varadigmaisnann@gmail.com>
2024-05-04 11:01:27 +0700

    Menambah If

--- Struct/Struct.cpp
+++ Struct/Struct.cpp
@@ -16,6 +16,57 @@ void addNode() {
 	cin >> newNode->noMhs;
 	cout << "\nEnter the name of the student: ";
 	cin >> newNode->name; 
+
+	if (START == NULL || newNode->noMhs <= START->noMhs)
+	{
+		if (START != NULL && newNode->noMhs == START->noMhs)
+		{
+			cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+			return;
+		}
+
+		newNode->next = START;
+		if (START != NULL)
+		{
+			START->prev = newNode;
+		}
+		newNode->prev = NULL;
+		START = newNode;
+
+	}
+	else 
+	{
+		Node* current = START;
+		Node* previous = NULL;
+
+		while (current != NULL && current->noMhs < newNode->noMhs)
+		{
+			previous = current;
+			current = current->next;
+		}
+
+		newNode->next = current;
+		newNode->prev = previous;
+
+		if (current != NULL)
+		{
+			current->prev = newNode;
+		}
+
+		if (previous != NULL)
+		{
+			previous->next = newNode;
+		}
+
+		else
+		{
+			START = newNode;
+		}
+			
+
+			
+		
+	}
 }
 
 int main()
